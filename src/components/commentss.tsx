9a2c3fd4ee17afcb9e65f24c8e5ef381ec7b9a12
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2024-03-19 19:18:20 -0300

    a

---
+++ src/components/commentss.tsx
@@ -0,0 +1,44 @@
+
+const Comments = () => {
+    return (
+        <div className="container-coments">
+            <div className="text-container">
+                <h1>Project Management</h1>
+            </div>
+
+            <div className="cards-container">
+                <div className="container-img-1">
+                    <p>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>
+                    <hr />
+                    <div className="info-user">
+                        <img src='' alt="img" />
+                        <h3>Oberon Shaw, MCH</h3>
+                        <p>Head of Talent Acquisition, North America</p>
+                    </div>
+
+                </div>
+                <div className="container-img-2">
+                    <p>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>
+                    <hr />
+                    <div className="info-user">
+                        <img src='' alt="img" />
+                        <h3>Oberon Shaw, MCH</h3>
+                        <p>Head of Talent Acquisition, North America</p>
+                    </div>
+                </div>
+                <div className="container-img-3">
+                    <p>Whitepate is designed as a collaboration tool for businesses that is a full project management solution.</p>
+                    <hr />
+                    <div className="info-user">
+                        <img src='' alt="img" />
+                        <h3>Oberon Shaw, MCH</h3>
+                        <p>Head of Talent Acquisition, North America</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+export default Comments;
